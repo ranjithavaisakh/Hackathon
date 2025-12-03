479e462f48011a9532c1c41c484ba4382e150bcb
ranjithavaisakh <153228806+ranjithavaisakh@users.noreply.github.com>
2025-12-03 13:59:58 +0530

    Update DemoApplication.java

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import java.time.OffsetDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-
+// Demo app
 public class DemoApplication {
 
 	public static void main(String[] args) {
